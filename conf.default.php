4cfbec2265f6d402e4133e04a0acf8ce2e7ac371
jodimas <jung-chris@gmx.de>
2012-11-28 22:03:51 +0100

    Beispiel config-datei

---
+++ conf.default.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Configuration Database
+*/
+define('DB_PASSWORD', '');
+define('DB_USER', '');
+define('DB_NAME', '');
+define('DB_HOST', '');
+
+
+/*
+ * Configuration URLs
+*/
+// the Base-URL of the frontend-website
+define('WEBSITE_URL' , 'http://mysite.com/');
+
+// the Base-URL which points in mvc
+define('SERVER_ROOT' , '/path/to/mvc');
+
+
+?>
