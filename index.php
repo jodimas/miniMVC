aa8295a77e6dc260007844909d26b5baeca0013b
jodimas <jung-chris@gmx.de>
2012-11-28 20:38:51 +0100

    startseite

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ * Start-Page of the MVC
+ **/
+
+// configuration  file
+require_once 'conf.php';
+
+/**
+ * Fetch the router
+ */
+require_once('controllers/' . 'router.php');
+
+
+?>
