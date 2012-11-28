1315ffb5c434d8dfa5df563900ea9afbd5cd7f8d
jodimas <jung-chris@gmx.de>
2012-11-28 20:26:14 +0100

    initiales conf - file

---
+++ conf.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Configuration Database
+*/
+define('DB_PASSWORD', '');
+define('DB_USER', '');
+define('DB_NAME', '');
+define('DB_HOST', '');
+
+
+/*
+ * Configuration URLs
+*/
+// the Base-URL of the frontend-website
+define('WEBSITE_URL' , '');
+
+// the Base-URL which points in mvc
+define('SERVER_ROOT' , '');
+
+?>
