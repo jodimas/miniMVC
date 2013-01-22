b31f4da72cb6d257f6bd9b202294b7c1e8aa9958
jodimas <jung-chris@gmx.de>
2013-01-22 17:28:56 +0100

    new comments

--- controllers/index.php
+++ controllers/index.php
@@ -5,19 +5,26 @@
 class Index_Controller
 {
 
+	// template festlegen
     public $template = 'index';
 
     public function main(array $getVars) {
-
+   		
+    	// indexModel intantiieren
     	$indexModel = new Index_Model;
 
+    	// view instantieren
 		$view = new View_Model($this->template);
-		
+
+		// daten holen
 		$title = $indexModel->getTitle();
 		$content = $indexModel->getContent();
-		
+
+		// daten an view schicken
 		$view->assign('title' , $title);
 		$view->assign('content' , $content);
 
     }
 }
+
+?>
